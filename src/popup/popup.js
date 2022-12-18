ce0974ba7f3671001799c9358dadb0717d4d992d
vincent5682 <vincentm04@live.com>
2022-12-18 18:16:54 +0100

    added slide animtaion

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -2,6 +2,11 @@ $('#refreshBtn').click(function() {
     spin()
 })
 
+$(document).ready(function() {
+    $('#slider').addClass('animate-enter animate-enter-active')
+})
+
+
 function spin() {
     $('#refreshBtn').addClass("animate-spin")
     setTimeout(() => {
